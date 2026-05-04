79fc0af37f440ed1eed432d8a118437fe774a859
izumiiu <peerawat.ez@gmail.com>
2026-05-05 02:05:39 +0700

    Object State

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,42 @@
 import { useState } from "react";
 
+interface Employee {
+  id: number;
+  name: string;
+  salary: number;
+}
+
 function App() {
   // ใส่ Generic เพื่อกำหนดว่าเป็น string (ใส่หรือไม่ใส่ก็ได้)
-  const [name, setName] = useState<string>("Tjay");
-  const [salary, setSalary] = useState<number>(30000);
-  const [isVisible, setIsvisible] = useState<boolean>(true);
-  const [newName, setNewname] = useState<string>("");
+ 
+ 
+  const [person, setPerson] = useState<Employee>({
+    id:100,
+    name:"Tjay",
+    salary:300000
+  });
+   const [isVisible, setIsvisible] = useState<boolean>(true);
+
 
   return (
     <>
+    <button onClick={() => setIsvisible(!isVisible)}>{isVisible ? "ซ่อน" : "แสดง"}</button>
       {isVisible && (
         <div>
-          <h2>ชื่อพนักงาน : {name}</h2>
-          <h2>เงินเดือน {salary} บาท</h2>
+          <h2>รหัสพนักงาน {person.id}</h2>
+          <h2>ชื่อพนักงาน {person.name}</h2>
+          <h2>เงินเดือน {person.salary} บาท</h2>
+          {/* น Arrow Function ถ้าเราต้องการส่งค่ากลับ (Return) เป็น Object ทันที เราต้องครอบด้วยวงเล็บกลมครับ 
+          เพื่อไม่ให้คอมพิวเตอร์สับสนว่าปีกกานั้นเป็นขอบเขตของฟังก์ชันหรือเป็นตัว Object */}
+          <button onClick={() => setPerson(data  => ({
+            ...data,
+            name:"boss",
+            salary:200000
+          }))}>แก้ใข</button>
         </div>
       )}
-      <hr />
-      <input  onChange={(e) => setNewname(e.target.value)} />
-      <button onClick={() => setName(newName)}>ยืนยัน</button>
-      <button onClick={() => setSalary((salary + 1000))}>เพิ่ม</button>
-      <button onClick={() => setSalary((salary - 1000))}>ลด</button>
-      <button onClick={() => setIsvisible(!isVisible)}>{isVisible ? "ซ่อน" : "แสดง"}</button>
-    </>
+      <hr/>
+       </>
   );
 }
 
